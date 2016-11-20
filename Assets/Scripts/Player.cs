c884e4bf13798c67c04d4a30441a82dce24aab2a
Yan Jardim <yan.guilherme72@gmail.com>
2016-11-20 20:41:43 -0200

    fix player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,34 +6,50 @@ using UnityEngine.Networking;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Player : NetworkBehaviour {
 	public float speed;
-	public int tileRange;
-	public Vector2 direction;
-	public bool canMove;
+	public float tileRange;
+	public Vector2 direction, lastPosition;
+	public bool canMove, left, right, up, down;
+
+    public PlayerDetect detectLeft, detectRight, detectUp, detectDown;
 
 	// Use this for initialization
 	void Start () {
-		tileRange = 1;
 		canMove = true;
-	}
+        left = right = up = down = true;
+        detectLeft = transform.FindChild("Left").GetComponent<PlayerDetect>();
+        detectRight = transform.FindChild("Right").GetComponent<PlayerDetect>();
+        detectUp = transform.FindChild("Up").GetComponent<PlayerDetect>();
+        detectDown = transform.FindChild("Down").GetComponent<PlayerDetect>();
+        //detect = transform.FindChild("Detect").gameObject;
+    }
 
 	void Update(){
+        left = detectLeft.collide;
+        right = detectRight.collide;
+        up = detectUp.collide;
+        down = detectDown.collide;
 
         if (canMove)
         {
-            if (Input.GetKey(KeyCode.A))
+            if (Input.GetKey(KeyCode.A) && !left)
             {
+                lastPosition = transform.position;
                 direction = new Vector2(transform.position.x - tileRange, transform.position.y);
+                
             }
-            if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.D) && !right)
             {
+                lastPosition = transform.position;
                 direction = new Vector2(transform.position.x + tileRange, transform.position.y);
             }
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.W) && !up)
             {
+                lastPosition = transform.position;
                 direction = new Vector2(transform.position.x, transform.position.y + tileRange);
             }
-            if (Input.GetKey(KeyCode.S))
+            if (Input.GetKey(KeyCode.S) && !down)
             {
+                lastPosition = transform.position;
                 direction = new Vector2(transform.position.x, transform.position.y - tileRange);
             }
         }
@@ -55,4 +71,12 @@ public class Player : NetworkBehaviour {
         GetComponent<Rigidbody2D>().MovePosition (Vector2.MoveTowards( transform.position, direction, speed));
         
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
+    
+
 }
