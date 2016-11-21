f4fe16eef6d3e55f9f00403d957589a6bdc6fe16
Yan Jardim <yan.guilherme72@gmail.com>
2016-11-20 22:12:46 -0200

    Enemy following player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+    public GameObject target;
+    public float speed;
+    public bool canMove, left, right, up, down;
+    private PlayerDetect detectLeft, detectRight, detectUp, detectDown;
+
+    public Vector2 direction;
+    // Use this for initialization
+    void Start () {
+        speed = GameManager.instance.enemySpeed;
+
+        direction.Set(0, 0);
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (target != null) //&& Vector2.Distance()
+        {
+            GetComponent<Rigidbody2D>().MovePosition(Vector2.MoveTowards(transform.position, target.transform.position, speed) );
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            target = other.gameObject;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            target = null;
+        }
+    }    
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,26 +4,32 @@ using UnityEngine.Networking;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
-public class Player : NetworkBehaviour {
-	public float speed;
-	public float tileRange;
-	public Vector2 direction, lastPosition;
-	public bool canMove, left, right, up, down;
+public class Player : NetworkBehaviour
+{
+    public float speed;
+    public float tileRange;
+    public Vector2 direction, lastPosition;
+    public bool canMove, left, right, up, down;
 
-    public PlayerDetect detectLeft, detectRight, detectUp, detectDown;
+    private PlayerDetect detectLeft, detectRight, detectUp, detectDown;
 
-	// Use this for initialization
-	void Start () {
-		canMove = true;
+    // Use this for initialization
+    void Start()
+    {
+        canMove = true;
         left = right = up = down = true;
         detectLeft = transform.FindChild("Left").GetComponent<PlayerDetect>();
         detectRight = transform.FindChild("Right").GetComponent<PlayerDetect>();
         detectUp = transform.FindChild("Up").GetComponent<PlayerDetect>();
         detectDown = transform.FindChild("Down").GetComponent<PlayerDetect>();
         //detect = transform.FindChild("Detect").gameObject;
+        GameManager.instance.AddPlayer(this.gameObject);
+
     }
 
-	void Update(){
+
+    void Update()
+    {
         left = detectLeft.collide;
         right = detectRight.collide;
         up = detectUp.collide;
@@ -35,7 +41,7 @@ public class Player : NetworkBehaviour {
             {
                 lastPosition = transform.position;
                 direction = new Vector2(transform.position.x - tileRange, transform.position.y);
-                
+
             }
             if (Input.GetKey(KeyCode.D) && !right)
             {
@@ -59,24 +65,30 @@ public class Player : NetworkBehaviour {
             canMove = false;
         }
         else canMove = true;
-			
-	}
 
-	// Update is called once per frame
-	void FixedUpdate () {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         if (!isLocalPlayer)
         {
             return;
         }
-        GetComponent<Rigidbody2D>().MovePosition (Vector2.MoveTowards( transform.position, direction, speed));
-        
+        GetComponent<Rigidbody2D>().MovePosition(Vector2.MoveTowards(transform.position, direction, speed));
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        
+
     }
 
-    
+    public override void OnStartLocalPlayer()
+    {
+        //GameManager.instance.AddPlayer(this.gameObject);
+    }
+
+
 
 }
